26ee2830cd01f8710455c9cb58929fabae81e2b7
Cs12krisz <csigerkrisztian@gmail.com>
2025-12-08 11:32:42 +0100

    Konzolos rész elkezdése

---
+++ negyszogCLI/Negyszog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negyszogCLI
+{
+    internal class Negyszog
+    {
+        public Negyszog(int a, int b, int c, int d)
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+
+        public Negyszog(string sor)
+        {
+            string[] temp = sor.Split(' ');
+            A = int.Parse(temp[0]);
+            B = int.Parse(temp[1]);
+            C = int.Parse(temp[2]);
+            D = int.Parse(temp[3]);
+        }
+
+        public int A { get; private set; }
+        public int B { get; private set; }
+        public int C { get; private set; }
+        public int D { get; private set; }
+    }
+}
---
+++ negyszogCLI/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace negyszogCLI
+{
+    internal class Program
+    {
+        static List<Negyszog> negyszogek = new List<Negyszog>();
+        static void Main(string[] args)
+        {
+            Beolvas("negyszogek.csv");
+        }
+
+        static void Beolvas(string fajl) 
+        {
+            using (StreamReader sr = new StreamReader(fajl))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    Negyszog negyszog = new Negyszog(sor);
+                    negyszogek.Add(negyszog);
+                }
+                
+            }
+        }
+    }
+}
